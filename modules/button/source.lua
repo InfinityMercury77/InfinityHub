4e303c28db2f472e23161e4f4c1fa3538de92e57
InfinityMercury77 <156934775+InfinityMercury77@users.noreply.github.com>
2024-12-01 10:14:30 -0300

    Update source.lua

--- modules/button/source.lua
+++ modules/button/source.lua
@@ -7,11 +7,13 @@ local Button = Instance.new("ScreenGui")
 local Close = Instance.new("TextButton")
 local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 local Icon = Instance.new("ImageButton")
+local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
 local UICorner = Instance.new("UICorner")
 local UICorner_2 = Instance.new("UICorner")
 local Open = Instance.new("TextButton")
-local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
+local UIAspectRatioConstraint_3 = Instance.new("UIAspectRatioConstraint")
 local Icon_2 = Instance.new("ImageButton")
+local UIAspectRatioConstraint_4 = Instance.new("UIAspectRatioConstraint")
 local UICorner_3 = Instance.new("UICorner")
 
 --Properties:
@@ -43,8 +45,11 @@ Icon.BackgroundTransparency = 1.000
 Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Icon.BorderSizePixel = 0
 Icon.Position = UDim2.new(0.214285716, 0, 0.142857149, 0)
-Icon.Size = UDim2.new(0, 32, 0, 34)
+Icon.Size = UDim2.new(0.571428597, 0, 0.693877578, 0)
 Icon.Image = "rbxassetid://10723415766"
+
+UIAspectRatioConstraint_2.Parent = Icon
+UIAspectRatioConstraint_2.AspectRatio = 0.941
 
 UICorner.Parent = Close
 
@@ -64,8 +69,8 @@ Open.Text = ""
 Open.TextColor3 = Color3.fromRGB(0, 0, 0)
 Open.TextSize = 14.000
 
-UIAspectRatioConstraint_2.Parent = Open
-UIAspectRatioConstraint_2.AspectRatio = 1.143
+UIAspectRatioConstraint_3.Parent = Open
+UIAspectRatioConstraint_3.AspectRatio = 1.143
 
 Icon_2.Name = "Icon"
 Icon_2.Parent = Open
@@ -74,34 +79,37 @@ Icon_2.BackgroundTransparency = 1.000
 Icon_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Icon_2.BorderSizePixel = 0
 Icon_2.Position = UDim2.new(0.214285716, 0, 0.142857149, 0)
-Icon_2.Size = UDim2.new(0, 32, 0, 34)
+Icon_2.Size = UDim2.new(0.571428597, 0, 0.693877578, 0)
 Icon_2.Image = "rbxassetid://10723415766"
+
+UIAspectRatioConstraint_4.Parent = Icon_2
+UIAspectRatioConstraint_4.AspectRatio = 0.941
 
 UICorner_3.Parent = Open
 
 -- Scripts:
 
-local function SCWKA_fake_script() -- Close.LocalScript 
+local function HROGFLC_fake_script() -- Close.LocalScript 
 	local script = Instance.new('LocalScript', Close)
 
 	local button = script.Parent
 	local open = script.Parent.Parent.Open 
-	button.MouseButton1Click:Connect(function()
+	button.TouchTap:Connect(function()
 		game:GetService('VirtualInputManager'):SendKeyEvent(true, "K", false, lmy)
 		button.Visible = false
 		open.Visible = true
 	end)
 end
-coroutine.wrap(SCWKA_fake_script)()
-local function ZYYRVS_fake_script() -- Open.LocalScript 
+coroutine.wrap(HROGFLC_fake_script)()
+local function HGWTFX_fake_script() -- Open.LocalScript 
 	local script = Instance.new('LocalScript', Open)
 
 	local button = script.Parent
 	local close = script.Parent.Parent.Close 
-	button.MouseButton1Click:Connect(function()
+	button.TouchTap:Connect(function()
 		game:GetService('VirtualInputManager'):SendKeyEvent(true, "K", false, lmy)
 		button.Visible = false
 		close.Visible = true
 	end)
 end
-coroutine.wrap(ZYYRVS_fake_script)()
+coroutine.wrap(HGWTFX_fake_script)()
