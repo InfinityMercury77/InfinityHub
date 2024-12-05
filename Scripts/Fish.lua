ddb6415125e70964ccff1da989d445ec1388f5f9
InfinityMercury77 <156934775+InfinityMercury77@users.noreply.github.com>
2024-12-04 22:08:36 -0300

    Update Fish.lua

--- Scripts/Fish.lua
+++ Scripts/Fish.lua
@@ -139,18 +139,14 @@ local Toggle = FischTab:CreateToggle({
    Callback = function(bool)
       autoshake = bool
       while autoshake do task.wait()
-         for _, v in pairs(game:GetService('Players').LocalPlayer.PlayerGui:GetChildren()) do
-            if v:IsA('ScreenGui') and v.Name == 'shakeui' then
-               for _, x in pairs(v:GetDescendants()) do
-                  if x:IsA('ImageButton') and x.Name == 'button' then
-                     game:GetService('GuiService').SelectedObject = x
-                     wait(.0001)
-                     vim:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-                     vim:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-                     wait(.0001)
-                     game:GetService('GuiService').SelectedObject = nil
-                  end
-               end
+         for _, Descendant in pairs(game:GetService('Players').LocalPlayer.PlayerGui:GetDescendants()) do
+            if Descendant.Name == 'button' and Descendant.Parent.Name == 'safezone' then
+               wait()
+               game:GetService('GuiService').SelectedObject = Descendant
+               vim:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+               vim:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+               wait()
+               game:GetService('GuiService').SelectedObject = nil
             end
          end
       end
